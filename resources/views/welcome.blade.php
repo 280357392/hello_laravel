52f2e9e009bf600b1d5a83ff70cf6496b2368a6b
Meng Wei <yyy333567@163.com>
2019-09-19 00:33:54 +0000

    Procfile for Heroku i love you

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel love
+                    Laravel i love you
                 </div>
 
                 <div class="links">
