8230f8df07cf90974f35f4141566a3c18a371ea4
Meng Wei <yyy333567@163.com>
2019-09-23 08:06:48 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-  <p>主页</p>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 Laravel 项目主页。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
