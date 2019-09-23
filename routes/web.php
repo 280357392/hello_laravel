c3a01d2db7bcb9443774cd63f894856bb11efb66
Meng Wei <yyy333567@163.com>
2019-09-23 08:44:28 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@
 //    return view('welcome');
 //});
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+//配置路由、命名路由
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
