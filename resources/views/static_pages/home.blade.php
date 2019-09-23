dffa2c5759cc6b5cce31bab7abe0ba3cd7f251de
Meng Wei <yyy333567@163.com>
2019-09-23 03:19:27 +0000

    完成基础页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Weibo App</title>
-</head>
-<body>
-  <h1>主页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title','主页标题哈')
+@section('content')
+  <p>主页666</p>
+@stop
